06bbae8e1e43ebbd76b64e2111f8f198e597516d
joelpangop <joelstephane.pangoptchomeni@west.com>
2021-02-11 14:11:50 -0500

    first commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(public platform: Platform) {
+    console.log(platform.platforms());
+  }
 
 }
